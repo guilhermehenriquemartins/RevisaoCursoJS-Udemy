add559a1cdbf9a3849a947d8fee844a78d099335
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-10-24 12:06:51 -0300

    em andamento

--- 34. Exercise to function, array and object/script.js
+++ 34. Exercise to function, array and object/script.js
@@ -1,6 +1,8 @@
 function cadastrar() {
   let form = window.document.getElementById('form1')
   let res = window.document.querySelector('div#res')
+  let erro1 = form.querySelector('div#erro1')
+  let erro2 = form.querySelector('div#erro2')
   let nome = form.querySelector('#inome')
   let sobrenome = form.querySelector('#isobrenome')
   let peso = form.querySelector('#ipeso')
@@ -9,6 +11,19 @@ function cadastrar() {
 
   form.addEventListener('submit', function(evento){
     evento.preventDefault();
+
+    if(nome.value.length == 0) {
+      erro1.innerHTML = 'O campo nome está vazio, preencha o campo antes de continuar'
+      erro1.style.marginTop = '-5px'
+      erro1.style.marginBottom = '5px'
+      erro1.style.color = 'red'
+      nome.style.outlineColor = 'red'
+    }
+    if(sobrenome.value.length == 0) {
+      erro2.innerHTML = 'O campo sobrenome está vazio, preencha o campo antes de continuar'
+      erro2.style.marginTop = '-5px'
+      erro2.style.marginBottom = '5px'
+    }
 
     cadastro.push(
       {
