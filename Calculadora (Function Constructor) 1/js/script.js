5823b6a3703284b60373bc81334b02ffecb286ac
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-11-27 10:32:48 -0300

    Revision 1 (Calculator - Constructor Function) 👍

---
+++ Calculadora (Function Constructor) 1/js/script.js
@@ -0,0 +1,60 @@
+function Calculadora() {
+  const display = document.querySelector('.display')
+
+  this.inicia = function() {
+    cliqueBotao()
+  }
+
+  const cliqueBotao = function() {
+    document.addEventListener('click', e => {
+      const el = e.target
+
+      if (el.classList.contains('btn-num')) {
+        btnParaDisplay(el.innerText)
+      }
+
+      if (el.classList.contains('btn-clear')) {
+        clearDisplay()
+      }
+
+      if (el.classList.contains('btn-del')) {
+        apagaUm()
+      }
+
+      if (el.classList.contains('btn-eq')) {
+        retornaResultado()
+      }
+    })
+  }
+
+  const btnParaDisplay = function(valor) {
+    display.value += valor
+  }
+
+  const clearDisplay = function() {
+    display.value = ''
+  }
+
+  const apagaUm = function() {
+    display.value = display.value.slice(0,-1)
+  }
+
+  const retornaResultado = function() {
+    let conta = display.value
+
+    try {
+      conta = eval(conta)
+
+      if(!conta) {
+        alert('Conta Inválida')
+        return
+      }
+      display.value = conta
+    } catch(e) {
+      alert('Conta Inválida')
+    }
+  }
+}
+
+const calculadora = new Calculadora()
+calculadora.inicia()
