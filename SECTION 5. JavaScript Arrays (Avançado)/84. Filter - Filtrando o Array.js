5914fbd3ec5a75a8d7ec05a118d3ba99fb12aa46
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-12-05 14:45:46 -0300

    Revision Class 84 (DONE)

--- SECTION 5. JavaScript Arrays (Avançado)/84. Filter - Filtrando o Array.js
+++ SECTION 5. JavaScript Arrays (Avançado)/84. Filter - Filtrando o Array.js
@@ -1,7 +1,6 @@
 //Retorne as pessoas que tem o nome com 5 letras ou mais
 //Retorne as pessoas com mais de 50 anos
 //Retorne as pessoas cujo nome termina com 'a'
-
 const pessoas = [
   { nome: 'Luiz', idade: 62 },
   { nome: 'Maria', idade: 23 },
@@ -11,12 +10,8 @@ const pessoas = [
   { nome: 'Wallace', idade: 47 }
 ]
 
-const pessoasCom5Letras = pessoas.filter(valor => valor.nome.length >= 5)
-const pessoasMais50Anos = pessoas.filter(valor => valor.idade > 50)
+const nome5Letras = pessoas.filter(valor => valor.nome.length >= 5)
+const mais50Anos = pessoas.filter(valor => valor.idade > 50)
 const terminaComA = pessoas.filter(valor => valor.nome.toLowerCase().endsWith('a'))
 
 console.log(terminaComA)
-
-/*
-OBS: Filter retorna um array do mesmo tamanho do array original ou menor, pois como o seu próprio nome já diz, ele é Filter (filtra) um array.
-*/
