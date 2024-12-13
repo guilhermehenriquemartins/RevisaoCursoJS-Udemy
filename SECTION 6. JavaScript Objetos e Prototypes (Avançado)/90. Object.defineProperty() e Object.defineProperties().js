d0200a4c790de4675ca9606a5f0e93f6993edb51
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-12-13 10:59:49 -0300

    Revision Class 90/91 (DONE)

--- SECTION 6. JavaScript Objetos e Prototypes (Avançado)/90. Object.defineProperty() e Object.defineProperties().js
+++ SECTION 6. JavaScript Objetos e Prototypes (Avançado)/90. Object.defineProperty() e Object.defineProperties().js
@@ -2,26 +2,11 @@ function Produto(nome, descricao, preco, estoque) {
   this.nome = nome
   this.descricao = descricao
   Object.defineProperties(this, {
-    preco: {
-      enumerable: true,
-      get: function() {
-        return preco
-      },
-
-      set: function(valor) {
-        if(typeof valor !== 'number') {
-          throw new TypeError('Valor Inválido')
-        }
-        preco = valor
-      },
-      configurable: false
-    },
     estoque: {
       enumerable: true,
       get: function() {
         return estoque
       },
-      
       set: function(valor) {
         if(typeof valor !== 'number') {
           throw new TypeError('Valor Inválido')
@@ -29,11 +14,24 @@ function Produto(nome, descricao, preco, estoque) {
         estoque = valor
       },
       configurable: false
+    },
+    preco: {
+      enumerable: true,
+      get: function() {
+        return preco
+      },
+      set: function(valor) {
+        if(typeof valor !== 'number') {
+          throw new TypeError('Valor Inválido')
+        }
+        preco = valor
+      },
+      configurable: false
     }
   })
 }
 
-const Violao = new Produto('Violão Acústico Konig Para Destro Classic Verniz Corda Nylon', 'Desfrute com este violão Konig da conexão com a música. Ele é projetado para amadores e profissionais. Com este instrumento você vai descobrir novos acordes, cantar suas canções e vai desfrutar da vida musical. Cordas de nylon e mede 39" (99,06cm)', 249.90, 5)
-Violao.estoque = 10
+const MousePad = new Produto('Mousepad Personalizado Com Logo Estampa Marca', 'Características: Base emborrachada antiderrapante - Superficie em tecido Impressão em Sublimação - cores vivas e ótima resolução - Alta durabilidade, estampa não desbota do tecido. Dimensões aproximadas: 17,5cm x 21,5cm', 22.90, 50)
+MousePad.preco = 25.90
 
-console.log(Violao.estoque)
+console.log(Object.keys(MousePad))
