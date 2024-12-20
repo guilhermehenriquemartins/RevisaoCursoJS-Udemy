4966bd36f48e2b8f3c90af6498a37e948b1346ac
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-12-20 09:21:33 -0300

    Revision 1 Class 98 (DONE)

--- ValidaCPF (Exercicio 1)/script.js
+++ ValidaCPF (Exercicio 1)/script.js
@@ -6,9 +6,9 @@ function ValidaCPF(cpfEnviado) {
   })
 }
 ValidaCPF.prototype.valida = function() {
-  if(typeof this.cpfLimpo === 'undefined') return false
-  if(this.cpfLimpo.length !== 11) return false
-  if(this.isSequencia()) return false
+  if (typeof this.cpfLimpo === 'undefined') return false
+  if (this.cpfLimpo.length !== 11) return false
+  if (this.isSequencia()) return false
 
   const cpfParcial = this.cpfLimpo.slice(0, -2)
   const digito1 = this.criaDigito(cpfParcial)
@@ -18,10 +18,10 @@ ValidaCPF.prototype.valida = function() {
 }
 ValidaCPF.prototype.criaDigito = function(cpfParcial) {
   const cpfArray = Array.from(cpfParcial)
-  let contador = cpfArray.length + 1
+  let regressivo = cpfArray.length + 1
   const total = cpfArray.reduce((acumulador, valor) => {
-    acumulador += (contador * Number(valor))
-    contador--
+    acumulador += (regressivo * Number(valor))
+    regressivo--
     return acumulador
   }, 0)
 
@@ -33,5 +33,5 @@ ValidaCPF.prototype.isSequencia = function() {
   return novoCPF === this.cpfLimpo
 }
 
-const cpf = new ValidaCPF('111.111.111-11')
+const cpf = new ValidaCPF('597.449.968-97')
 console.log(cpf.valida())
