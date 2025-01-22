913c555dbef637d2139526446b38ec82d3ec3ccd
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-01-22 11:52:09 -0300

    Revision of Class 107 (DONE)

---
+++ Formulario (2)/assets/js/validarCPF.js
@@ -0,0 +1,38 @@
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      enumerable: false,
+      value: cpfEnviado.replace(/\D+/g, ''),
+      writable: false,
+      configurable: false
+    })
+  }
+
+  eSequencia() {
+    return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo
+  }
+
+  valida() {
+    if(typeof this.cpfLimpo === 'undefined') return false
+    if(this.cpfLimpo.length !== 11) return false
+    if(this.eSequencia()) return false
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2)
+    const digito1 = this.criaDigito(cpfParcial)
+    const digito2 = this.criaDigito(cpfParcial + digito1)
+    const novoCPF = cpfParcial + digito1 + digito2
+    return novoCPF === this.cpfLimpo
+  }
+
+  criaDigito(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial)
+    let regressivo = cpfArray.length + 1
+    const total = cpfArray.reduce((acumulador, valor) => {
+      acumulador += (regressivo * Number(valor))
+      regressivo--
+      return acumulador
+    }, 0)
+    const digito = 11 - (total % 11)
+    return digito > 9 ? '0' : String(digito)
+  } 
+}
