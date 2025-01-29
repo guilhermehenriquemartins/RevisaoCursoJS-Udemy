fc4aa57017acfefb147fef977e26c483128b56f1
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-01-29 12:32:54 -0300

    Revision of Class 89 (DONE)

--- SECTION 6. JavaScript Objetos e Prototypes (Avançado)/89. Revisando Objetos.js
+++ SECTION 6. JavaScript Objetos e Prototypes (Avançado)/89. Revisando Objetos.js
@@ -1,10 +1,21 @@
-const pessoa = new Object()
-pessoa.nome = 'Guilherme'
-pessoa.sobrenome = 'Henrique'
-pessoa.idade = 25
-pessoa.getDataNascimento = function() {
-  const dataAtual = new Date()
-  return dataAtual.getFullYear() - this.idade
+//COMO CRIAMOS OBJETO LITERAL
+const pessoa = {
+  nome: 'Guilherme',
+  sobrenome: 'Henrique'
 }
 
-console.log(pessoa.getDataNascimento())
+//COMO CRIAMOS UM OBJETO ATRAVÉS DO SEU CONSTRUTOR
+const pessoa2 = new Object()
+pessoa2.nome = 'Guilherme'
+pessoa2.idade = 26
+pessoa2.sobrenome = 'Henrique'
+pessoa2.nomeCompleto = function() {
+  console.log(`${this.nome} ${this.sobrenome}`) 
+}
+pessoa2.getDataNascimento = function() {
+  const dataAtual = new Date()
+  console.log(`${dataAtual.getFullYear() - this.idade}`) 
+}
+
+pessoa2.nomeCompleto()
+pessoa2.getDataNascimento()
