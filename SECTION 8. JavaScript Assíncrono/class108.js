ee1ae6e1422e1b9532524e0a326451511456f85e
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-02-04 10:27:06 -0300

    Revision of Class 108 (DONE)

--- SECTION 8. JavaScript Assíncrono/class108.js
+++ SECTION 8. JavaScript Assíncrono/class108.js
@@ -1,10 +1,10 @@
 function rand(min, max) {
   min *= 1000
   max *= 1000
-  return Math.floor(Math.random() * (max - min) + min)
+  return Math.floor(Math.random() * (min - max) + min)
 }
 
-function esperaAi(msg, tempo) {
+function dados(msg, tempo) {
   return new Promise((resolve, reject) => {
     if(typeof msg !== 'string') reject('VALOR INVÁLIDO')
 
@@ -14,20 +14,17 @@ function esperaAi(msg, tempo) {
   })
 }
 
-esperaAi('Frase 1', rand(1, 3))
+dados('Frase 1', rand(1, 3))
   .then(resposta => {
     console.log(resposta)
-    return esperaAi('Frase 2', rand(1, 3))
+    return dados('Frase 2', rand(1, 3))
   })
   .then(resposta => {
     console.log(resposta)
-    return esperaAi(1234, rand(1, 3))
+    return dados(1234, rand(1, 3))
   })
   .then(resposta => {
     console.log(resposta)
-  })
-  .then(() => {
-    console.log('ACABOU-SE!')
   })
   .catch(resposta => {
     console.log('ERRO: ', resposta)
