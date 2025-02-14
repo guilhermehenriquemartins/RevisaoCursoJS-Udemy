d0bc6d85e72702f6906f6069b3e0ff1d4bcd21f0
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-02-14 16:14:05 -0300

    Revision of Class 111 and 112 3rd time (DONE)

---
+++ SECTION 8. JavaScript Assíncrono/class 112 (4)/assets/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('click', e => {
+  const el = e.target
+  const tag = el.tagName.toLowerCase()
+
+  if(tag === 'a') {
+    e.preventDefault()
+    carregaPagina(el)
+  }
+})
+
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href')
+    const response = await fetch(href)
+
+    if(response.status !== 200) throw new Error('ERRO 404 NOSSO!')
+
+    const resultado = await response.text()
+    mostraResultado(resultado)
+  } catch(e) {
+    console.error(e)
+  } 
+}
+
+function mostraResultado(response) {
+  const resultado = document.querySelector('.resultado')
+  resultado.innerHTML = response
+}
